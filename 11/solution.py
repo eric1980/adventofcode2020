44f0492737c66055cc7d98e8c903d3fb2ee5d7c0
Eric Fornstedt <eric.fornstedt@gmail.com>
2020-12-11 17:19:25 +0100

    Add soluton 2 day 11

--- 11/solution.py
+++ 11/solution.py
@@ -5,8 +5,8 @@ def run_iteration(seats, check_function, occupied_limit):
     new_seat_map = deepcopy(seats)
     has_changed = False
 
-    for row in range(1, len(seats)-1):
-        for col in range(1, len(seats[0])-1):
+    for row in range(1, len(seats) - 1):
+        for col in range(1, len(seats[0]) - 1):
 
             occupied = check_function(seats, row, col)
 
@@ -21,14 +21,33 @@ def run_iteration(seats, check_function, occupied_limit):
 
 
 def adjacent_occupied(seats, row, col):
-    return seats[row-1][col-1:col+2].count('#') + \
-           (seats[row][col-1] + seats[row][col+1]).count('#') + \
-           seats[row+1][col-1:col+2].count('#')
+    return seats[row - 1][col - 1:col + 2].count('#') + \
+        (seats[row][col - 1] + seats[row][col + 1]).count('#') + \
+        seats[row + 1][col - 1:col + 2].count('#')
 
 
 def visible_occupied(seats, row, col):
-    # Dummy
-    return 1
+    directions = [(-1, -1), (-1,  0), (-1,  1),
+                  ( 0, -1),           ( 0,  1),
+                  ( 1, -1), ( 1,  0), ( 1,  1)]
+
+    return sum([get_visible_seat(seats, row, col, d) == '#' for d in directions])
+
+
+def get_visible_seat(seats, row, col, direction):
+    delta_row, delta_col = direction
+    check_row = row + delta_row
+    check_col = col + delta_col
+    max_cols = len(seats[0])
+    max_rows = len(seats)
+
+    while seats[check_row][check_col] == '.':
+        if not 0 < check_row < max_rows - 1 or not 0 < check_col < max_cols - 1:
+            break
+        check_row += delta_row
+        check_col += delta_col
+
+    return seats[check_row][check_col]
 
 
 def count_stable_seats(seats, check_function, occupied_limit):
@@ -39,16 +58,11 @@ def count_stable_seats(seats, check_function, occupied_limit):
     return sum([row.count('#') for row in seats])
 
 
-def print_seat_map(seats):
-    for row in seats:
-        print(''.join(row))
-
-
 def main():
     # Get data and add 'border'
     seats = [list('.' + value.strip() + '.') for value in open('input.txt', 'r').readlines()]
-    seats.insert(0, '.'*len(seats[0]))
-    seats.append('.'*len(seats[0]))
+    seats.insert(0, '.' * len(seats[0]))
+    seats.append('.' * len(seats[0]))
 
     test_data = [
         list('............'),

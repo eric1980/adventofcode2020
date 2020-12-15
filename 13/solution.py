b60e9b3bc056e8d14e45d02318925fed5e9eeba5
Eric Fornstedt <eric.fornstedt@gmail.com>
2020-12-15 23:48:34 +0100

    Add solution 2 day 13

--- 13/solution.py
+++ 13/solution.py
@@ -1,5 +1,7 @@
+from math import prod
+
 def find_solution_one(earliest_time, bus_ids):
-    bus_departures = dict([((earliest_time // bus_id + 1) * bus_id, bus_id) for bus_id in bus_ids])
+    bus_departures = dict([((earliest_time // int(bus_id) + 1) * int(bus_id), int(bus_id)) for bus_id in bus_ids if bus_id != 'x'])
 
     best_time = min([key for key in bus_departures])
     best_bus = bus_departures[best_time]
@@ -7,28 +9,42 @@ def find_solution_one(earliest_time, bus_ids):
     return best_bus * (best_time - earliest_time)
 
 
+def find_solution_two(bus_ids):
+    mods = [(int(v)-i) % int(v) for i, v in enumerate(bus_ids) if v != 'x']
+    busses = [int(bus_id) for bus_id in bus_ids if bus_id != 'x']
+    factors = [prod(busses) // bus_id for bus_id in busses]
+
+    for i, factor in enumerate(factors):
+        multiplier = 1
+        while (factor * multiplier) % busses[i] != mods[i]:
+            multiplier += 1
+        factors[i] = factor * multiplier
+
+    return sum(factors) % prod(busses)
+
+
 def main():
     with open('input.txt', 'r') as input_file:
         earliest_time = int(input_file.readline())
-        bus_ids = [int(id) for id in input_file.readline().split(',') if id != 'x']
+        bus_ids = [id for id in input_file.readline().split(',')]
 
     test_earliest_time = 939
-    test_bus_ids = [7, 13, 59, 31, 19]
+    test_bus_ids = ['7', '13', 'x', 'x', '59', 'x', '31', '19']
 
     test_result_one = find_solution_one(test_earliest_time, test_bus_ids)
-    # test_result_two = find_solution_two(data)
+    test_result_two = find_solution_two(test_bus_ids)
 
     print(f'Test result 1: {test_result_one}')
-    # print(f'Test result 2: {test_result_two}')
+    print(f'Test result 2: {test_result_two}')
 
     assert test_result_one == 295, 'Test 1 does not pass'
-    # assert test_result_two == 26, 'Test 2 does not pass'
+    assert test_result_two == 1068781, 'Test 2 does not pass'
 
     solution_one = find_solution_one(earliest_time, bus_ids)
-    # solution_two = find_solution_two(data)
+    solution_two = find_solution_two(bus_ids)
 
     print(f'Solution 1: {solution_one}')
-    # print(f'Solution 2: {solution_two}')
+    print(f'Solution 2: {solution_two}')
 
 
 if __name__ == "__main__":

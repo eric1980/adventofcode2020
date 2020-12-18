2df91a9351c7cbacdae70468654a20125b9fe3c4
Eric Fornstedt <eric.fornstedt@gmail.com>
2020-12-18 09:59:50 +0100

    Add solution 2 day 17

--- 17/solution.py
+++ 17/solution.py
@@ -1,63 +1,63 @@
 from collections import defaultdict
 from copy import deepcopy
 
+
 class PocketDimension():
     def __init__(self, initialisation, margin=10, use_forth_dimension=False):
+        self.use_dimension = use_forth_dimension
+        self.number_of_dimensions = 1 if not self.use_dimension else margin * 2 + 1
 
-        self.dimension = [[list([0] * (len(initialisation[0]) + margin * 2))
+        self.dimension = [[[list([0] * (len(initialisation[0]) + margin * 2))
             for i in range((len(initialisation) + margin * 2))]
             for i in range(margin*2+1)]
+            for i in range(self.number_of_dimensions)]
 
-        self.origo = {'x': len(self.dimension[0][0]) // 2, 'y': len(self.dimension[0]) // 2, 'z': len(self.dimension) // 2}
+        self.origo = {'x': len(self.dimension[0][0][0]) // 2, 'y': len(self.dimension[0][0]) // 2,
+                      'z': len(self.dimension[0]) // 2, 'w': len(self.dimension) // 2}
 
         plane_x = self.origo['x'] - len(initialisation[0]) // 2
         plane_y = self.origo['y'] - len(initialisation) // 2
-        plane_z = 0
 
         for y, row in enumerate(initialisation):
             for x, cell in enumerate(row):
-                self.dimension[self.origo['z']][y+plane_y][x+plane_x] = cell
+                self.dimension[self.origo['w']][self.origo['z']][y + plane_y][x + plane_x] = cell
 
     def cycle(self):
         new_dimension = deepcopy(self.dimension)
-        for z in range(1, len(self.dimension)):
-            for y in range(1, len(self.dimension[0])):
-                for x in range(1, len(self.dimension[0][0])):
-                    #new_dimension[z][y][x] = self.get_active_surrounding(x, y, z)
-                    active_cells = self.get_active_surrounding(x, y, z)
-                    if new_dimension[z][y][x] == 1:
-                        if active_cells not in (3, 4):
-                            new_dimension[z][y][x] = 0
-                    elif new_dimension[z][y][x] == 0 and active_cells == 3:
-                        new_dimension[z][y][x] = 1
+
+        for w in range(1, len(self.dimension)) if self.use_dimension else [0]:
+            for z in range(1, len(self.dimension[0])):
+                for y in range(1, len(self.dimension[0][0])):
+                    for x in range(1, len(self.dimension[0][0][0])):
+                        active_cells = self.get_active_surrounding(x, y, z, w)
+                        if new_dimension[w][z][y][x] == 1 and active_cells not in (3, 4):
+                            new_dimension[w][z][y][x] = 0
+                        elif new_dimension[w][z][y][x] == 0 and active_cells == 3:
+                            new_dimension[w][z][y][x] = 1
 
         self.dimension = deepcopy(new_dimension)
 
-    def get_active_surrounding(self, x, y, z):
+    def get_active_surrounding(self, x, y, z, w):
         active = 0
-        for plane in self.dimension[z-1:z+2]:
-            for row in plane[y-1:y+2]:
-                active += row[x-1:x+2].count(1)
+        for dimension in self.dimension[w-1:w+2] if self.use_dimension else self.dimension:
+            for plane in dimension[z-1:z+2]:
+                for row in plane[y-1:y+2]:
+                    active += row[x-1:x+2].count(1)
 
         return active
 
     def count_active_cubes(self):
         active = 0
-        for plane in self.dimension:
-            for row in plane:
-                active += row.count(1)
+        for dimension in self.dimension:
+            for plane in dimension:
+                active += sum([row.count(1) for row in plane])
 
         return active
-
-def print_dim(data):
-    for plane in data:
-        for row in plane:
-            print(row)
-        print('')
 
 
 def get_active_cubes(data, iterations, use_forth_dimension=False):
     pocket_dimension = PocketDimension(data, use_forth_dimension=use_forth_dimension)
+
     for _ in range(iterations):
         pocket_dimension.cycle()
 
